aae6546345908c395abd4515905e461cd2c1cb33
Lucas del Pozo <cldelpozo@futurespace.es>
2019-11-19 12:00:49 +0100

    Aproximación de formularios | Template

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -16,16 +16,26 @@ export class TemplateComponent {
   usuario: any = {
     nombre: '',
     apellido: '',
-    email: ''
+    email: '',
+    paises: '',
+    sexo: '',
+    acepta: false
   };
+
+  paises = [{codigo: 'CRI', nombre: 'Costa Rica'},
+            {codigo: 'ESP', nombre: 'España'},
+            {codigo: 'AN', nombre: 'Andorra'}];
+
+  sexos: string[] = ['Hombre', 'Mujer', 'NonBinari'];
   constructor() { }
 
-  
+
 
   guardar(forma: NgForm) {
     console.log('Formulario posteado');
     console.log(forma);
     console.log('valor: ', forma.value);
     console.log('usuario', this.usuario);
+    console.log('pais', this.usuario.paises);
   }
 }
